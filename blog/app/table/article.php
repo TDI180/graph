7ea0eaf1e8be999696016fd0b681dd572608a55d
captain <captainflyers11@gmail.com>
2021-06-13 01:12:13 +0200

    test-creation-branch

--- blog/app/table/article.php
+++ blog/app/table/article.php
@@ -3,13 +3,16 @@ namespace App\Table;
 
 class article {
 	           
-				 /* La surcharge magique en PHP permet de "créer" dynamiquement des propriétés et des méthodes. 
+				/*TEST DE LA BRANCHE */
+
+				/* La surcharge magique en PHP permet de "créer" dynamiquement des propriétés et des méthodes. 
 				    Ces entités dynamiques sont traitées via des méthodes magiques établies que l'on peut positionner dans une classe pour divers types d'actions.
                     Les méthodes magiques de surcharge sont appelées lors de l'interaction avec des propriétés ou des méthodes qui n'ont pas été déclarées 
 				    ou ne sont pas visibles dans le contexte courant. 
 				    Le reste de cette section utilise les termes de "propriétés inaccessibles" et de "méthodes inaccessibles"
 				    pour se référer à cette combinaison de déclaration et de visibilité. 
 				 */
+				 
 				/*
 				   __set() est sollicitée lors de l'écriture de données vers des propriétés inaccessibles (protégées ou privées) ou non existante. 
 				*/ 
