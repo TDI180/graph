3abb601e4771ec94387c54abf2b4ccd838997518
captain <captainflyers11@gmail.com>
2021-06-13 00:00:52 +0200

    third-commit

--- blog/app/table/article.php
+++ blog/app/table/article.php
@@ -3,9 +3,20 @@ namespace App\Table;
 
 class article {
 	           
+				 /* La surcharge magique en PHP permet de "créer" dynamiquement des propriétés et des méthodes. 
+				    Ces entités dynamiques sont traitées via des méthodes magiques établies que l'on peut positionner dans une classe pour divers types d'actions.
+                    Les méthodes magiques de surcharge sont appelées lors de l'interaction avec des propriétés ou des méthodes qui n'ont pas été déclarées 
+				    ou ne sont pas visibles dans le contexte courant. 
+				    Le reste de cette section utilise les termes de "propriétés inaccessibles" et de "méthodes inaccessibles"
+				    pour se référer à cette combinaison de déclaration et de visibilité. 
+				 */
+				/*
+				   __set() est sollicitée lors de l'écriture de données vers des propriétés inaccessibles (protégées ou privées) ou non existante. 
+				*/ 
 				 
 				 public function __get($key) //methode magik a revoir
-				 {
+				 
+				   {
 					var_dump ($key);
 					
 					$method = 'get'.ucfirst($key);	
@@ -13,7 +24,7 @@ class article {
 					//var_dump ($this);
 					//echo '-------------------';
 					
-					$key=$this->$method();
+					$key=$this->$method();/* on rajoute les accolade et ca donne un appel de getURL() ou getExtrait()*/
 					//echo '*****************';
 					//var_dump ($key);
 					//echo '*****************';
